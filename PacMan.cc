7af393f5d296d7422894953b775d6d127e1cdf87
rodriguezma <33692368+rodriguezma@users.noreply.github.com>
2017-11-21 13:31:37 +0100

    arreglos

--- PacMan.cc
+++ PacMan.cc
@@ -14,6 +14,35 @@
  SoLoud::Soloud canal;
  //Declaración variables canal audio.
  SoLoud::Wav ejemplo1,ejemplo2;
+ 
+ esat::SpriteHandle SpriteSheet;
+ esat::SpriteHandle Mapa;
+ 
+ struct casillas{
+	int x,y;
+	bool muro;
+}casilla[28][31];                       
+ 
+void CargaSprites(){
+	
+	SpriteSheet = esat::SpriteFromFile("./Recursos/Imagenes/sprite.png");
+	Mapa = esat::SubSprite(SpriteSheet,0,0,448,492);
+	
+}
+
+void InitTablero(){
+	
+	int a=448/28;
+	int b=492/31;
+	
+	for(int i=0;i<28;i++){
+		for(int j=0;j<31;j++){
+			casilla[i][j].x=a*i;
+			casilla[i][j].y=b*j;
+		}
+	}
+
+}
 
 
 int esat::main(int argc, char **argv) {
@@ -27,8 +56,10 @@ int esat::main(int argc, char **argv) {
   ejemplo1.load("./Recursos/Audio/ogg/dp_frogger_extra.ogg");
   ejemplo2.load("./Recursos/Audio/ogg/dp_frogger_start.ogg");
  
-  esat::WindowInit(800,600);
+  esat::WindowInit(448,492);
   WindowSetMouseVisibility(true);
+  
+  CargaSprites();
   
 
   while(esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
@@ -36,7 +67,9 @@ int esat::main(int argc, char **argv) {
 	
 	esat::DrawBegin();
     esat::DrawClear(0,0,0);
-   
+	
+	esat::DrawSprite(Mapa,0,0);
+	
     if(esat::IsSpecialKeyPressed(esat::kSpecialKey_Left)){
       canal.play(ejemplo1); //Inicia reproducción canal
     }
